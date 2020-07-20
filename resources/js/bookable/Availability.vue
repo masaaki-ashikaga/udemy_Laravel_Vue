b878bb2abc893fc2c7b8a236eb9912e894885414
masaaki-ashikaga <gamasaga0312@gmail.com>
2020-07-21 00:03:32 +0900

    HTTP

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">Check Availability</h6>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label form="from">From</label>
+                <input
+                    type="text"
+                    name="from"
+                    class="form-control form-control-sm"
+                    placeholder="Start date"
+                    v-model="from"
+                    @keyup.enter="check"
+                >
+            </div>
+
+            <div class="form-group col-md-6">
+                <label form="to">To</label>
+                <input
+                    type="text"
+                    name="to"
+                    class="form-control form-control-sm"
+                    placeholder="End date"
+                    v-model="to"
+                    @keyup.enter="check"
+                >
+            </div>
+            <button class="btn btn-secondary btn-block" @click="check">Check!</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            from: null,
+            to: null,
+        }
+    },
+    methods: {
+        check() {
+            alert('I will check something now !');
+        }
+    }
+}
+</script>
+
+<style scoped>
+    label {
+        font-size: 0.7rem;
+        text-transform: uppercase;
+        color: gray;
+        stroke-width: bolder;
+    }
+</style>
